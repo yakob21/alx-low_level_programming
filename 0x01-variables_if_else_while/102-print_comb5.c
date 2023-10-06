b49401453dd675d1127e191c78bd4f7829755de6
yakob <yakobgetachew19@gmail.com>
2023-10-06 12:43:09 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,7 +16,7 @@ int main(void)
 			putchar(' ');
 			putchar((q / 10) + '0');
 			putchar((q % 10) + '0');
-			if (p == 98 && q == 98)
+			if (p == 98 && q == 99)
 				continue;
 			putchar(',');
 			putchar(' ');
