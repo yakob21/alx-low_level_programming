0196774ebb218fc7965bd6c8004206df1b52a3f3
yakob <yakobgetachew19@gmail.com>
2023-10-06 11:17:31 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - the main function
+ * Return: always  0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		if (i == 9)
+			putchar(i + '0');
+		else
+		{
+		       	putchar(i + '0');
+               		putchar(',');
+	         	putchar(' ');
+		}
+	}
+	return (0);
+}
