6881d0cc52c43e022981eaa749121f18dd15540f
yakob <yakobgetachew19@gmail.com>
2023-10-06 04:35:29 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,19 +8,21 @@
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n>0)
+
+	if (n > 0)
 	{
-		printf("%d number is positive\n", n);
+	printf("%d is positive\n", n);
 	}
-	else if (n==0)
+	else if (n == 0)
 	{
-       	        printf("%d number is zero\n", n);
+       	printf("%d is zero\n", n);
 	}
 	else
 	{
-                printf("%d number is negative\n", n);
+        printf("%d is negative\n", n);
 	}
 	return (0);
 }
