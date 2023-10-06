8dc778f2bc9c7587d1073532436dbea417c5ab46
yakob <yakobgetachew19@gmail.com>
2023-10-06 10:27:09 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - the main fuction
+ * Return: always 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'z' ; i >= 'a' ; i--)
+	putchar(i);
+	putchar('\n');
+	return (0);
+
+}
