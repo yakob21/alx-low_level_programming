96494941ff9dec85459497d9a24e28386729e511
yakob <yakobgetachew19@gmail.com>
2023-10-15 22:23:00 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
-// Author By Eng Ismail
-// URL = https://github.com/macaanto14
+
 /**
- 
+ * Author By Eng Ismail
+ * URL = https://github.com/macaanto14
  * @n: An integer input pointer
  */
 void reset_to_98(int *n)
