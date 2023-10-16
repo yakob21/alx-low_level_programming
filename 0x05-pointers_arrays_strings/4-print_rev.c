9274f61f48ab1f5453594ce9eff2e780a6a47b26
yakob <yakobgetachew19@gmail.com>
2023-10-16 00:09:09 -0700

    ./4-print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev- print reversed string, followed by a new line
+ * @s: pointer to the string to point
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int longi = 0;
+	int o;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	s--;
+	for (o = longi; o > 0; o--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
